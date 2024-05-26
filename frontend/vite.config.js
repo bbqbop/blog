a7d5cae7d7276f1be7d4746a8f3e570b8b23c327
bbqbop <dierk.peters@gmail.com>
2024-05-26 15:48:56 -0400

    try dist instead of build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from "dotenv";
 
+dotenv.config();
+
 export default defineConfig(() => {
   return {
-    build: {
-      outDir: 'build',
-    },
     plugins: [react()],
     define: {
         'process.env.VITE_PORT':JSON.stringify(process.env.VITE_PORT)
